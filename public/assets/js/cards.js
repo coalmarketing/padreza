0c9124fa9b3ffec446b20c5455676621f00ddd73
Bousheck <slesingr.michal@gmail.com>
2025-12-23 01:36:19 +0100

    date fix

--- public/assets/js/cards.js
+++ public/assets/js/cards.js
@@ -1,22 +1,19 @@
-
-const cards = document.querySelectorAll('.card');
-const infos = document.querySelectorAll('.info');
-
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        const key = card.dataset.key;
-        const info = document.querySelector(`.info[data-key="${key}"]`);
-
-        const isActive = card.classList.contains('active');
-
-        // reset všeho
-        cards.forEach(c => c.classList.remove('active'));
-        infos.forEach(i => i.classList.remove('active'));
-
-        // pokud nebyla aktivní, aktivuj
-        if (!isActive) {
-            card.classList.add('active');
-            info.classList.add('active');
-        }
+(() => {
+  // src/assets/js/cards.js
+  var cards = document.querySelectorAll(".card");
+  var infos = document.querySelectorAll(".info");
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const key = card.dataset.key;
+      const info = document.querySelector(`.info[data-key="${key}"]`);
+      const isActive = card.classList.contains("active");
+      cards.forEach((c) => c.classList.remove("active"));
+      infos.forEach((i) => i.classList.remove("active"));
+      if (!isActive) {
+        card.classList.add("active");
+        info.classList.add("active");
+      }
     });
-});
+  });
+})();
+//# sourceMappingURL=cards.js.map
